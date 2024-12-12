cfe29bbf1aa061be60e3a5eabdefa93ec901a2e5
hasanisokay <hasanisokay@gmail.com>
2024-12-13 01:05:29 +0600

    added redirect in logout function

--- src/utils/logOut.mjs
+++ src/utils/logOut.mjs
@@ -1,12 +1,12 @@
 'use server'
 import { TOKEN_COOKIE } from "@/constants/constantNames.mjs";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 const logOut = async () => {
     let cookieStore = await cookies();
-    cookieStore.delete(TOKEN_COOKIE);
-  
-
+    cookieStore.delete(TOKEN_COOKIE);  
+    return NextResponse.redirect("/login");
 };
 
 export default logOut;
